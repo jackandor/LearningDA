a6882781807be7bd74ab58401067d154829b44dd
jack cao <19173347@qq.com>
2015-08-14 15:47:57 +0800

    fix incorrect variable name

--- compare.R
+++ compare.R
@@ -9,10 +9,10 @@ tj100 <- tj100[order(tj100$Date),]
 cmp_df <- merge(i100, tj100, by='Date')
 
 start <- 1
-cmp_df$i100_index_income.x <- (cmp_df$Close.x / cmp_df[start, 'Close.x'] - 1) * 100
-cmp_df$i100_fund_income.x <- (cmp_df$Net.x /cmp_df[start, 'Net.x'] - 1) * 100
-cmp_df$tj100_index_income.y <- (cmp_df$Close.y / cmp_df[start, 'Close.y'] - 1) * 100
-cmp_df$tj100_fund_income.y <- (cmp_df$Net.y / cmp_df[start, 'Net.y'] - 1) * 100
+cmp_df$i100_index_income <- (cmp_df$Close.x / cmp_df[start, 'Close.x'] - 1) * 100
+cmp_df$i100_fund_income <- (cmp_df$Net.x /cmp_df[start, 'Net.x'] - 1) * 100
+cmp_df$tj100_index_income <- (cmp_df$Close.y / cmp_df[start, 'Close.y'] - 1) * 100
+cmp_df$tj100_fund_income <- (cmp_df$Net.y / cmp_df[start, 'Net.y'] - 1) * 100
 
 max4p <- max(cmp_df$i100_index_income, cmp_df$i100_fund_income, cmp_df$tj100_index_income, cmp_df$tj100_fund_income)
 min4p <- min(cmp_df$i100_index_income, cmp_df$i100_fund_income, cmp_df$tj100_index_income, cmp_df$tj100_fund_income)
