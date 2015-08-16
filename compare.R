3898307ea37d218294a3afc66c0d7c75f9504840
jack cao <19173347@qq.com>
2015-08-17 00:46:07 +0800

    Add candidate(Fund Code:210004).

--- compare.R
+++ compare.R
@@ -18,9 +18,15 @@ ss000001 <- read.table("ss000001.csv", sep=',', header=TRUE, stringsAsFactors=FA
 ss000001$Date <- as.Date(ss000001$Date, "%Y-%m-%d")
 ss000001 <- rename(ss000001, c(Close="ss000001_close"))
 
+candidate <- read.table("candidate.csv", sep=',', header=TRUE, stringsAsFactors=FALSE)
+candidate$Date <- as.Date(candidate$Date, "%Y-%m-%d")
+candidate <- rename(candidate, c(Net="candidate_net"))
+
+
 cmp_df <- merge(i100, tj100, by='Date')
 cmp_df <- merge(cmp_df, hs300, by='Date')
 cmp_df <- merge(cmp_df, ss000001, by='Date')
+cmp_df <- merge(cmp_df, candidate, by='Date')
 
 cmp_df <- cmp_df[order(cmp_df$Date),]
 
@@ -33,9 +39,11 @@ cmp_df$tj100_fund_income <- (cmp_df$tj100_net / cmp_df[start, 'tj100_net'] - 1) 
 cmp_df$hs300_index_income <-(cmp_df$hs300_close / cmp_df[start, 'hs300_close'] - 1) * 100
 cmp_df$hs300_fund_income <- (cmp_df$hs300_net / cmp_df[start, 'hs300_net'] - 1) * 100
 cmp_df$ss000001_index_income <- (cmp_df$ss000001_close / cmp_df[start, 'ss000001_close'] - 1) * 100
+cmp_df$candidate_fun_income <- (cmp_df$candidate_net / cmp_df[start, 'candidate_net'] - 1) * 100
 
-max4p <- max(cmp_df$i100_index_income, cmp_df$i100_fund_income, cmp_df$tj100_index_income, cmp_df$tj100_fund_income, cmp_df$hs300_index_income, cmp_df$hs300_fund_income, cmp_df$ss000001_index_income)
-min4p <- min(cmp_df$i100_index_income, cmp_df$i100_fund_income, cmp_df$tj100_index_income, cmp_df$tj100_fund_income, cmp_df$hs300_index_income, cmp_df$hs300_fund_income, cmp_df$ss000001_index_income)
+
+max4p <- max(cmp_df$i100_index_income, cmp_df$i100_fund_income, cmp_df$tj100_index_income, cmp_df$tj100_fund_income, cmp_df$hs300_index_income, cmp_df$hs300_fund_income, cmp_df$ss000001_index_income, cmp_df$candidate_fun_income)
+min4p <- min(cmp_df$i100_index_income, cmp_df$i100_fund_income, cmp_df$tj100_index_income, cmp_df$tj100_fund_income, cmp_df$hs300_index_income, cmp_df$hs300_fund_income, cmp_df$ss000001_index_income, cmp_df$candidate_fun_income)
 
 plot(cmp_df$i100_index_income, type='b', col='red', xaxt='n', xlab='Date', ylab='Income', ylim=c(min4p, max4p))
 lines(cmp_df$tj100_index_income, type='b', col='blue')
@@ -43,5 +51,6 @@ lines(cmp_df$i100_fund_income, type='b', col='pink')
 lines(cmp_df$tj100_fund_income, type='b', col='grey')
 lines(cmp_df$hs300_index_income, type='b', col='maroon')
 lines(cmp_df$hs300_fund_income, type='b', col='yellow')
-lines(cmp_df$ss000001_index_income, typ='b', col='green')
+lines(cmp_df$ss000001_index_income, type='b', col='green')
+lines(cmp_df$candidate_fun_income, type='b', col='black')
 axis(1, 1:length(cmp_df$Date), labels=cmp_df$Date)
