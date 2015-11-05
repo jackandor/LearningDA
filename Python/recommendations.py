63ab6d61683264bc7e7d2fe7d9e7868c4ef79fae
jack cao <19173347@qq.com>
2015-11-05 23:18:18 +0800

    Add sim_distance(...) function in recommendations.py

--- Python/recommendations.py
+++ Python/recommendations.py
@@ -41,3 +41,13 @@ critics = {
         'Snakes on a Plane': 4.5,
         'You, Me and Dupree': 1.0,
         'Superman Returns': 4.0 }}
+
+from math import sqrt
+def sim_distance(prefs, person1, person2):
+    si={}
+    for item in prefs[person1]:
+        if item in prefs[person2]:
+            si[item]=1
+    if len(si)==0: return 0
+    sum_of_squares=sum([pow(prefs[person1][item]-prefs[person2][item], 2) for item in prefs[person1] if item in prefs[person2]])
+    return 1/(1+sqrt(sum_of_squares))
